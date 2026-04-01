fd9897201f1935e432974afc6be7bdc26020c72f
abdalazim151 <abdalazim.ahmed11111@gmail.com>
2026-04-01 13:42:00 +0200

    update cart model

--- models/cart.js
+++ models/cart.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const favoriteSchema = new mongoose.Schema({
+const cart = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'users',
@@ -11,12 +11,16 @@ const favoriteSchema = new mongoose.Schema({
         ref: 'products',
         required: [true, 'Favorite must have a product']
     },
+    quantity:{
+        type:Number,
+        required:true
+    },
     status:{
         type:String,
         enum:['not-confirmed','pending','accepted','rejected'],
         default:'not-confirmed'
     }
 });
-favoriteSchema.index({ user: 1, product: 1, status:1 }, { unique: true });
-const Favorite = mongoose.model('favorites', favoriteSchema);
-export default Favorite;
+cart.index({ user: 1, product: 1, status:1 }, { unique: true });
+const Cart = mongoose.model('cars', favoriteSchema);
+export default Cart;
